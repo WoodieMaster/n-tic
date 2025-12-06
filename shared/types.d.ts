06b69869df9fc08144fba02ae3e17fb0cfa61ee1
MasterWoodie <tabletzuhausebeiuns@gmail.com>
2025-12-06 12:29:27 +0100

    add most of the required message types

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -1,8 +1,65 @@
 export type WsServerMessage = {
-    type: "join"
+    type: "playerJoin",
+    playerName: string,
+} | {
+    type: "setup"
+} | {
+    type: "roomSetup",
+    admin: string,
+    players: string[]
+} | {
+    type: "playerLeave",
+    playerName: string
+} | {
+    type: "nextTurn",
+    nextPlayer: string,
+    board: BoardCell[]
+} | {
+    type: "roomSettings",
+    dimensionCount?: number,
+    sideLength?: number,
+    playerShapes?: Record<string, Shape>,
+} | {
+    type: "gameEnd",
+    winner: string,
+    board: BoardCell[],
+    reason: {
+        type: "board",
+        winVec: BoardVec,
+        winPosition: BoardPosition
+    } | {
+        type: "opponentsDisconnected"
+    }
+} | {
+    type: "error",
+    msg: string
 }
 
-export type WsClientMessage = {}
+export type WsClientMessage = {
+    type: "setup",
+    reconnectId: string | null,
+} | {
+    type: "joinRoom",
+    roomId: string,
+    playerName: string | null
+} | {
+    type: "createRoom",
+    playerName: string | null
+} | {
+    type: "leaveRoom",
+} | {
+    type: "selectShape",
+    shape: string
+} | {
+    type: "editSettings",
+    dimensionCount?: number,
+    sideLength?: number
+} | {
+    type: "startGame"
+} | {
+    type: "place",
+    position: BoardPosition
+}
 
 export interface User {
     id: string,
@@ -13,3 +70,5 @@ export type BoardCell = 0 | 1 | 2;
 
 export type BoardVec = (-1 | 0 | 1)[];
 export type BoardPosition = number[];
+
+export type Shape = "cross" | "circle" | "square" | "triangle"
