0ef1bd8efe2a8969502ad6777bf654a04c22938e
MasterWoodie <tabletzuhausebeiuns@gmail.com>
2026-01-19 08:07:47 +0100

    display proper player count

--- frontend/src/components/GameHeader.tsx
+++ frontend/src/components/GameHeader.tsx
@@ -1,15 +1,14 @@
 import {Box, Typography} from "@mui/material";
+import useRoomState from "../stores/useRoomState.ts";
 
-interface Props {
-    roomId: string;
-    players: string[]
-}
 
-const GameHeader = (p: Props) => {
+const GameHeader = () => {
+    const {players, roomId} = useRoomState();
+
     return (
         <Box sx={{bgcolor: "rgb(30,30,30)", display: "flex", padding: 2, gap: 10}}>
-            <Typography variant="h5">Join using: <b>{p.roomId}</b></Typography>
-            <Typography variant={"h5"}>{p.players.length}/2 players</Typography>
+            <Typography variant="h5">Join using: <b>{roomId}</b></Typography>
+            <Typography variant={"h5"}>{players.length}/2 players</Typography>
         </Box>
     );
 };
